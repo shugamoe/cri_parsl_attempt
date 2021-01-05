458297b4ec25d64399802f230626346e60ddadc5
Julian McClellan <juliancmcclellan@gmail.com>
2021-01-05 17:26:47 -0600

    New parsl issues

--- config.py
+++ config.py
@@ -1,31 +1,112 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from parsl.config import Config
+from parsl.data_provider.files import File
 from parsl.channels import LocalChannel
 from parsl.executors import HighThroughputExecutor
 from parsl.providers import TorqueProvider
 from parsl.addresses import address_by_route, address_by_query, address_by_hostname
 from parsl.launchers import SingleNodeLauncher
+from parsl.utils import get_all_checkpoints
+from parsl.dataflow.memoization import id_for_memo
+
 
 sbayesr_config = Config(
+    checkpoint_mode = "task_exit",
+    checkpoint_files = get_all_checkpoints(),
     executors=[
         HighThroughputExecutor(
-            label="gardner_plink_1ppn",
+            label="standard_16gb_1core",
             cores_per_worker=1,
-            #  address=address_by_hostname(), # Gets cri16in001 as address
-            #  address=address_by_query(), # Gets 128.135.245.36 as address
-            address=address_by_route(), # Gets 10.50.178.250 as address
-            # address="128.135.245.36",
+            address=address_by_hostname(), # Gets cri16in001 as address
+            worker_debug = False,
             provider=TorqueProvider(
                 channel=LocalChannel(), # Default if not given
-                # launcher=SingleNodeLauncher(),
-                worker_init="module load gcc/6.2.0; module load plink/2.0; module load python/3.8.1; python3 -c 'import parsl; print(parsl.__version__)'",
+                launcher=SingleNodeLauncher(),
+                worker_init=("module load gcc/6.2.0; module load plink/2.0; "
+                            "cd {0}; pwd; "
+                            "module load python/3.8.1; "
+                            "python3 -c 'import parsl; print(parsl.__version__)'; "
+                            "export PATH=~/.local/bin:$PATH; "
+                            "export PYTHONPATH='{0}:{{PYTHONPATH}}'").format(os.getcwd()),
+                walltime="6:00:00",
+                scheduler_options="#PBS -l mem=16gb",
+                init_blocks=1,
+                min_blocks=0,
+                max_blocks=30,
+                nodes_per_block=1,
+                parallelism=1
+            ),
+        ),
+	HighThroughputExecutor(
+            label="plink2_express_2gb_1core",
+            cores_per_worker=1,
+            address=address_by_hostname(), # Gets cri16in001 as address
+            #  address=address_by_query(), # Gets 128.135.245.36 as address
+            # address=address_by_route(), # Gets 10.50.178.250 as address
+            # address="128.135.245.36",
+            worker_debug = True,
+            provider=TorqueProvider(
+                channel=LocalChannel(), # Default if not given
+                launcher=SingleNodeLauncher(),
+                worker_init=("module load gcc/6.2.0; module load plink/2.0; "
+                            "cd {0}; pwd; "
+                            "module load python/3.8.1; "
+                            "python3 -c 'import parsl; print(parsl.__version__)'; "
+                            "export PATH=~/.local/bin:$PATH; "
+                            "export PYTHONPATH='{0}:{{PYTHONPATH}}'").format(os.getcwd()),
                 walltime="01:00:00",
                 scheduler_options="#PBS -l mem=2gb",
+                init_blocks=0,
+                min_blocks=0,
+                max_blocks=45,
                 nodes_per_block=1,
-                max_blocks=1
+                parallelism=1
             ),
-        )
+        ),
+	HighThroughputExecutor(
+            label="plink1_express_2gb_1core",
+            cores_per_worker=1,
+            address=address_by_hostname(), # Gets cri16in001 as address
+            #  address=address_by_query(), # Gets 128.135.245.36 as address
+            # address=address_by_route(), # Gets 10.50.178.250 as address
+            # address="128.135.245.36",
+            worker_debug = True,
+            provider=TorqueProvider(
+                channel=LocalChannel(), # Default if not given
+                launcher=SingleNodeLauncher(),
+                worker_init=("module load gcc/6.2.0; module load plink/1.90; "
+                            "cd {0}; pwd; "
+                            "module load python/3.8.1; "
+                            "python3 -c 'import parsl; print(parsl.__version__)'; "
+                            "export PATH=~/.local/bin:$PATH; "
+                            "export PYTHONPATH='{0}:{{PYTHONPATH}}'").format(os.getcwd()),
+                walltime="01:00:00",
+                scheduler_options="#PBS -l mem=2gb",
+                init_blocks=0,
+                min_blocks=0,
+                max_blocks=45,
+                nodes_per_block=1,
+                parallelism=1
+            ),
+        ),
     ],
 )
+
+
+@id_for_memo.register(File)
+def id_for_memo_File(f, output_ref=False):
+    import os
+    if output_ref:
+        # logger.debug("hashing File as output ref without content: {}".format(f))
+        print("hashing File as output ref without content: {}".format(f))
+        return f.url
+    else:
+        # logger.debug("hashing File as input with content: {}".format(f))
+        print("hashing File as input with content: {}".format(f))
+        assert f.scheme == "file"
+        filename = f.filepath
+        stat_result = os.stat(filename)
+
--- library.py
+++ library.py
@@ -2,24 +2,86 @@
 # -*- coding: utf-8 -*-
 
 from parsl.app.app import bash_app
+from config import id_for_memo_File
+import parsl
+
+@bash_app(executors=["standard_16gb_1core"], cache=False, ignore_for_cache=["stdout", "stderr"])
+def filter_dose(ogeno, source, pval, avginfscore, maf, inputs=[], outputs=[],
+                stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
+    import os
+    PY_APP="/gpfs/data/gao-lab/Julian/gaolab_hub/apps/ragoc_to_ldpred2/ragoc_to_ldpred2.py"
+    geno, gwas = inputs
+
+    bash_args = {
+      "py_app": PY_APP,
+      "idir": os.path.dirname(geno.filepath), # Input directory
+      "ifile": os.path.basename(geno.filepath), # Input file
+      "osnp": None,
+      "ogeno": ogeno,
+      "source": source,
+      "gwas": gwas.filepath,
+      "pval": pval,
+      "avginfscore": avginfscore,
+      "maf": maf
+    }
+    bash_command = \
+    """
+    python3 {py_app} geno \
+      --idir "{idir}" \
+      --ipat "{ifile}" \
+      --osnp "{osnp}" \
+      --ogeno "{ogeno}" \
+      --source "{source}" \
+      --gwas "{gwas}" \
+      --pval "{pval}" \
+      --avginfscore "{avginfscore}" \
+      --maf "{maf}" \
+      --filter-only
+    """.format(**bash_args)
+    return bash_command
+    # return "python3 {py_app} geno -h".format(py_app=PY_APP)
+    
     
 
-@bash_app
-def dose_to_bbf(fam, out_dir, inputs=[], outputs=[], stdout="dose_to_bbf.stdout", stderr="dose_to_bbf.stderr"):
-    import os
+@bash_app(executors=["plink2_express_2gb_1core"], cache=True, ignore_for_cache=["stdout", "stderr"])
+def dose_to_bbf(fam, dose_file, outputs=[], stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
 
     bash_command = \
     """
     plink2 --fam {fam} \
-      --import-dosage {dose_file} noheader ref-first chr-col-num=2 pos-col-num=3 format=1 skip1=2 \ 
+      --import-dosage {dose_file} noheader ref-first chr-col-num=2 pos-col-num=3 format=1 skip1=2 \
       --import-dosage-certainty 0.1 \
       --make-bed \
       --geno 0.05 \
       --out {opath}
-    """.format(fam=fam, dose_file=inputs[0], opath=outputs[0])
+    """.format(fam=fam.filepath, dose_file=dose_file.filepath, opath=outputs[0].filepath.replace(".bed", ""))
+    return bash_command
+
+@bash_app(executors=["plink1_express_2gb_1core"])
+def write_bbf_parts_list(chr_num, geno_type, inputs=[], outputs=[], 
+                         stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
+    bash_command = \
+    """
+    ls -d ../input/processed_geno_{geno_type}/bbf_genotype/* | grep "chr{chr_num}.p" | \
+    sed -e 's/\.log//' -e 's/\.fam//' -e 's/\.bim//' -e 's/\.bed//' | \
+    sort --version-sort | uniq > {output.filepath}
+    """.format(chr_num=chr_num, geno_type=geno_type, output=outputs[0])
+    return bash_command
+
+@bash_app(executors=["plink1_express_2gb_1core"])
+def merge_bbf_parts(list_file, outputs=[], 
+                    stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
+    bash_command = \
+    """
+    plink --merge-list {list_file} --make-bed --out {out_prefix}
+    """.format(list_file=list_file.filepath, out_prefix=outputs[0].filepath.replace(".bed", ""))
     return bash_command
 
 @bash_app
-def echo_hello(stdout='echo-hello.stdout', stderr='echo-hello.stderr'):
+def echo_hello(stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
     return 'echo "Hello World!"'
+
+@bash_app
+def ll_file(inputs=[], stdout=parsl.AUTO_LOGNAME, stderr=parsl.AUTO_LOGNAME):
+    return 'ls -l {0}'.format(inputs[0])
 
